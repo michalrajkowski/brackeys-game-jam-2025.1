334109eb4053ec59efc409c97267c69f95a455f9
michalrajkowski <infernosynek@gmail.com>
2025-02-21 22:49:31 +0100

    removed legacy code with bloks handling. Changed it to respect blocks_handler

--- main.py
+++ main.py
@@ -46,21 +46,28 @@ def get_tile(tile_x, tile_y):
     return pyxel.tilemaps[1].pget(tile_x, tile_y)
 
 def destroy_block(tile_x,tile_y):
-    pyxel.tilemap(1).pset(tile_x, tile_y, VOID_TILE)
+    blocks_handler.destroy_block(tile_x, tile_y)
 
 def is_colliding(x, y, is_falling):
-    x1 = pyxel.floor(x) // 8
-    y1 = (pyxel.floor(y)+1) // 8
+    # Get player new bounding box:
+    x1 = pyxel.floor(x) // 8 
+    y1 = (pyxel.floor(y)) // 8
     x2 = (pyxel.ceil(x) + 7) // 8
     y2 = (pyxel.ceil(y) + 7) // 8
+
+    print(x1, y1, x2, y2)
+
+    # Check if player is completly in empty space!
     for yi in range(y1, y2 + 1):
         for xi in range(x1, x2 + 1):
-            if get_tile(xi, yi)[0] >= WALL_TILE_X:
+            if blocks_handler.is_solid(xi, yi):
                 return True
-    if is_falling and y % 8 == 1:
-        for xi in range(x1, x2 + 1):
-            if get_tile(xi, y1 + 1) == TILE_FLOOR:
-                return True
+    
+    # Legacy code!
+    # if is_falling and y % 8 == 1:
+    #     for xi in range(x1, x2 + 1):
+    #         if get_tile(xi, y1 + 1) == TILE_FLOOR:
+    #             return True
     return False
 
 def is_close_enough(entity_x, entity_y, block_x, block_y, proximity=2):
@@ -106,23 +113,8 @@ def push_back(x, y, dx, dy):
 
 
 def is_wall(x, y):
-    tile = get_tile(x // 8, y // 8)
-    return tile == TILE_FLOOR or tile[0] >= WALL_TILE_X
-
-
-def spawn_enemy(left_x, right_x):
-    left_x = pyxel.ceil(left_x / 8)
-    right_x = pyxel.floor(right_x / 8)
-    for x in range(left_x, right_x + 1):
-        for y in range(16):
-            tile = get_tile(x, y)
-            if tile == TILE_SPAWN1:
-                enemies.append(Enemy1(x * 8, y * 8))
-            elif tile == TILE_SPAWN2:
-                enemies.append(Enemy2(x * 8, y * 8))
-            elif tile == TILE_SPAWN3:
-                enemies.append(Enemy3(x * 8, y * 8))
-
+    block_x, block_y = x // 8, y // 8
+    return blocks_handler.is_solid(block_x, block_y)
 
 def cleanup_entities(entities):
     for i in range(len(entities) - 1, -1, -1):
@@ -235,11 +227,22 @@ class BlocksHandler:
         self.blocks_map: dict[tuple[int, int], BlockID] = {}
         self.generate_map()
 
+    def destroy_block(self, block_x, block_y):
+        if not self.is_in_range(block_x, block_y):
+            return
+        self.blocks_map[(block_x, block_y)] = BlockID.AIR
+
+    def is_solid(self, block_x, block_y) -> bool:
+        if not self.is_in_range(block_x, block_y):
+            return False
+        return Blocks.is_solid(self.get_block_id(block_x, block_y))
+
     def generate_map(self):
         for i in range(MAP_SIZE_BLOCKS_X):
             for j in range(MAP_SIZE_BLOCKS_Y):
-                if j > 10:
-                    self.blocks_map[(i, j)] = random.choice(list(BlockID))
+                if j > 6:
+                    self.blocks_map[(i, j)] = BlockID.DIRT
+                    # self.blocks_map[(i, j)] = random.choice(list(BlockID))
                 else:
                     self.blocks_map[(i, j)] = BlockID.AIR
     def is_in_range(self, block_x, block_y):
@@ -449,100 +452,6 @@ class Player:
         pyxel.blt(horizontal_block[0], horizontal_block[1], 0, *marker, TRANSPARENT_COLOR)
         pyxel.blt(block_down[0], block_down[1], 0, *marker, TRANSPARENT_COLOR)
 
-
-class Enemy1:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.dx = 0
-        self.dy = 0
-        self.direction = -1
-        self.is_alive = True
-
-    def update(self):
-        self.dx = self.direction
-        self.dy = min(self.dy + 1, 3)
-        if self.direction < 0 and is_wall(self.x - 1, self.y + 4):
-            self.direction = 1
-        elif self.direction > 0 and is_wall(self.x + 8, self.y + 4):
-            self.direction = -1
-        self.x, self.y = push_back(self.x, self.y, self.dx, self.dy)
-
-    def draw(self):
-        u = pyxel.frame_count // 4 % 2 * 8
-        w = 8 if self.direction > 0 else -8
-        pyxel.blt(self.x, self.y, 0, u, 24, w, 8, TRANSPARENT_COLOR)
-
-
-class Enemy2:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.dx = 0
-        self.dy = 0
-        self.direction = 1
-        self.is_alive = True
-
-    def update(self):
-        self.dx = self.direction
-        self.dy = min(self.dy + 1, 3)
-        if is_wall(self.x, self.y + 8) or is_wall(self.x + 7, self.y + 8):
-            if self.direction < 0 and (
-                is_wall(self.x - 1, self.y + 4) or not is_wall(self.x - 1, self.y + 8)
-            ):
-                self.direction = 1
-            elif self.direction > 0 and (
-                is_wall(self.x + 8, self.y + 4) or not is_wall(self.x + 7, self.y + 8)
-            ):
-                self.direction = -1
-        self.x, self.y = push_back(self.x, self.y, self.dx, self.dy)
-
-    def draw(self):
-        u = pyxel.frame_count // 4 % 2 * 8 + 16
-        w = 8 if self.direction > 0 else -8
-        pyxel.blt(self.x, self.y, 0, u, 24, w, 8, TRANSPARENT_COLOR)
-
-
-class Enemy3:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.time_to_fire = 0
-        self.is_alive = True
-
-    def update(self):
-        self.time_to_fire -= 1
-        if self.time_to_fire <= 0:
-            dx = player.x - self.x
-            dy = player.y - self.y
-            sq_dist = dx * dx + dy * dy
-            if sq_dist < 60**2:
-                dist = pyxel.sqrt(sq_dist)
-                enemies.append(Enemy3Bullet(self.x, self.y, dx / dist, dy / dist))
-                self.time_to_fire = 60
-
-    def draw(self):
-        u = pyxel.frame_count // 8 % 2 * 8
-        pyxel.blt(self.x, self.y, 0, u, 32, 8, 8, TRANSPARENT_COLOR)
-
-
-class Enemy3Bullet:
-    def __init__(self, x, y, dx, dy):
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
-        self.is_alive = True
-
-    def update(self):
-        self.x += self.dx
-        self.y += self.dy
-
-    def draw(self):
-        u = pyxel.frame_count // 2 % 2 * 8 + 16
-        pyxel.blt(self.x, self.y, 0, u, 32, 8, 8, TRANSPARENT_COLOR)
-
-
 class App:
     def __init__(self):
         pyxel.init(128, 128, title="2D Miner")
@@ -556,7 +465,6 @@ class App:
         input = InputHandler()
         mining_helper = MiningHelper()
         blocks_handler = BlocksHandler()
-        spawn_enemy(0, 127)
         pyxel.playm(0, loop=True)
         pyxel.run(self.update, self.draw)
 
@@ -581,8 +489,8 @@ class App:
         # Draw level
         pyxel.camera()
 
-        pyxel.bltm(0, 0, 2, (scroll_x // 4) % 128, (scroll_y // 4) % 128, 128, 128) # Background
-        pyxel.bltm(0, 0, 1, scroll_x, scroll_y, 128, 128, TRANSPARENT_COLOR) # Foreground
+        # pyxel.bltm(0, 0, 2, (scroll_x // 4) % 128, (scroll_y // 4) % 128, 128, 128) # Background
+        # pyxel.bltm(0, 0, 1, scroll_x, scroll_y, 128, 128, TRANSPARENT_COLOR) # Foreground
         
         # Render map with block handler:
         blocks_handler.draw()
@@ -613,7 +521,6 @@ def game_over():
     player.dx = 0
     player.dy = 0
     enemies = []
-    spawn_enemy(0, 127)
     pyxel.play(3, 9)
 
 
